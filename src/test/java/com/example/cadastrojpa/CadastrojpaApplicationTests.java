5ad7a5bd613db3bd069adede87282bec619e1ba2
Nykolas <nykolaskauansilva@gmail.com>
2025-08-05 14:42:53 -0300

    Updates

---
+++ src/test/java/com/example/cadastrojpa/CadastrojpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cadastrojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrojpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
